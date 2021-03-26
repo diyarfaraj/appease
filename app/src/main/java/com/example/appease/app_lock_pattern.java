47735126bca7127337f282e408ed4e975653b971
Diyar Faraj <diyar.faraj@gmail.com>
2021-03-26 22:12:20 +0100

    init

---
+++ app/src/main/java/com/example/appease/app_lock_pattern.java
@@ -0,0 +1,14 @@
+package com.example.appease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class app_lock_pattern extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_lock_pattern);
+    }
+}
